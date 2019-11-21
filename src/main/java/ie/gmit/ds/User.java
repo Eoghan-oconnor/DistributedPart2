cd5be2faef17f56669d52f62ea3011d1a01a570e
Eoghan O'Connor <oconnorjohn40@gmail.com>
2019-11-21 15:48:05 +0000

    Adding User Class

---
+++ src/main/java/ie/gmit/ds/User.java
@@ -0,0 +1,55 @@
+package ie.gmit.ds;
+
+public class User {
+
+	private int userId;
+	private String userName;
+	private String email;
+	private String password;
+	private String hashedpassword;
+	private String salt;
+
+	public User() {
+		super();
+		// Needed for Jackson deserialisation
+
+	}
+
+	public User(int userId, String userName, String email, String password) {
+		super();
+		this.userId = userId;
+		this.userName = userName;
+		this.email = email;
+		this.password = password;
+	}
+	
+	public int getUserId() {
+		return userId;
+		
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public String getHashedpassword() {
+		return hashedpassword;
+	}
+
+	public String getSalt() {
+		return salt;
+	}
+
+
+	
+	
+
+}
